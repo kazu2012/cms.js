8672127d476482674e1f22e157ab2076088458bb
kazu2012 <cat_shou@yahoo.co.jp>
2019-05-07 19:04:32 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -53,8 +53,8 @@ $(function() {
 
      // If Github mode is set, your Github username and repo name.
     githubUserSettings: {
-      username: 'yourusername',
-      repo: 'yourrepo'
+      username: 'kazu2012',
+      repo: 'cms.js-starter'
     },
 
     // If Github mode is set, choose which Github branch to get files from.
